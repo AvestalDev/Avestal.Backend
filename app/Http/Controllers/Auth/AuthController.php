06196e172a1a7467910144a2f7f08ab8a196536e
Aleksandr Koregin <the.koregin@yandex.ru>
2021-05-21 16:06:26 +0300

    Fix AuthController

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -109,7 +109,17 @@ class AuthController extends Controller {
     }
 
     public function refresh() {
-        return $this->respondWithToken(auth()->refresh());
+
+        $whitelist = Whitelist::where('access_token', JWTAuth::getToken())->first();
+
+        $token = JWTAuth::parseToken()->refresh();
+
+        $whitelist->access_token = $token;
+        $whitelist->save();
+
+        return response()->json([
+            'access_token' => $token
+        ], 200);
     }
 
     public function logout() {
